c6708226be1b81e4b04d025988390562f3bc9339
thamirisadrianodarocha <adrianosthamiris@gmail.com>
2021-11-01 21:15:35 -0300

    first-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+//tipando as variáveis
+
+export interface Car {
+
+    id: number;
+    model: string;
+    color: string;
+    price: number;
+}
